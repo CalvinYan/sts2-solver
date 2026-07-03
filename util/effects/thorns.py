19656a97316775c4b74414ccefd28c969aeb93ef
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 11:04:39 -0700

    Remove comment about thorns not handling multihits

--- util/effects/thorns.py
+++ util/effects/thorns.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Thorns(Effect):
-    # Doesn't handle multihits properly but that's fine for starter decks
     def resolve(self, move: Move, is_target: bool) -> None:
         if is_target:
             move.actor.take_damage(self.power)
