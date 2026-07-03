6ebd77a0cce4ba34676115f765bf15ead08899ef
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 03:05:10 -0700

    Add Seapunk enemy and encounter

--- character/enemy/encounter.py
+++ character/enemy/encounter.py
@@ -1,7 +1,9 @@
-from character.enemy.enemies import FuzzyWurmCrawler, Nibbit, ShrinkerBeetle
+from character.enemy.enemies import FuzzyWurmCrawler, Nibbit, Seapunk, ShrinkerBeetle
 
 def fuzzy_wurm_crawler(): return [FuzzyWurmCrawler()]
 
 def nibbit(): return [Nibbit()]
+
+def seapunk(): return [Seapunk()]
 
 def shrinker_beetle(): return [ShrinkerBeetle()]
---
+++ character/enemy/enemies/seapunk.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from character.enemy import Enemy, Intent
+from util import Action
+from util.effects import Strength
+
+@dataclass
+class Seapunk(Enemy):
+    intent = SeaKick()
+    min_hp = 47
+    max_hp = 49
+
+@dataclass
+class SeaKick(Intent):
+    actions = [Action(damage=13, block=0)]
+    
+    def next(self) -> Intent:
+        return SpinningKick()
+
+@dataclass
+class SpinningKick(Intent):
+    actions = [Action(damage=2, block=0)] * 4
+    
+    def next(self) -> Intent:
+        return BubbleBurp()
+
+@dataclass
+class BubbleBurp(Intent):
+    actions = [Action(damage=0, block=8, buffs=[Strength(power=2)])]
+    
+    def next(self) -> Intent:
+        return SeaKick()
