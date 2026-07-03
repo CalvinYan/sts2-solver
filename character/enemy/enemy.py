847863cbf64211f45daac0bb52627c140fd23246
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 02:06:24 -0700

    Implement data class Enemy

---
+++ character/enemy/enemy.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from random import randint
+
+@dataclass
+class Enemy(Character):
+    """
+    A hostile non-player character with an intent and a range of possible hp values.
+    Each fight starts with one or more enemies, and ends if none remain with more than 0 HP.
+    """
+    hp = field(init=False)
+
+    intent: Intent
+    min_hp: ClassVar[int]
+    max_hp: ClassVar[int]
+
+    def __post_init__(self):
+        self.hp = randint(self.min_hp, self.max_hp)
+
+    def resolve_start_of_turn(self) -> None:
+        self.intent = self.intent.next()
