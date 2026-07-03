6492f0b89523df7daaaa7a66d9e7c388897ba300
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 01:01:55 -0700

    Redefine an intent as a sequence of actions, not just one

--- character/enemy/intent.py
+++ character/enemy/intent.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Intent:
-    """An action that an enemy will perform on a given turn, with a random variable determining its next intent."""
-    action: Action
+    """A sequence of actions that an enemy will perform on a given turn, with a random variable determining its next intent."""
+    actions: list[Action]
 
     def next_intent(self) -> Intent:
         pass
