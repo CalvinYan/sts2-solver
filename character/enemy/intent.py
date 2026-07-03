028e2d4af1f786163633f440303b711c43edeaf3
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 02:05:48 -0700

    Rename Intent.next_intent to Intent.next

--- character/enemy/intent.py
+++ character/enemy/intent.py
@@ -5,5 +5,5 @@ class Intent:
     """A sequence of actions that an enemy will perform on a given turn, with a random variable determining its next intent."""
     actions: list[Action]
 
-    def next_intent(self) -> Intent:
+    def next(self) -> Intent:
         pass
