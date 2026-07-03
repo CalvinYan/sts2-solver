be476704910ffb6b5d31baa86d5a24d6df001a3b
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 00:54:28 -0700

    Redefine an action as a sequence of actions, not just one

--- util/move.py
+++ util/move.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Move:
-    """An action by one character upon another (can be themselves). Used for both player card plays and enemy turns."""
-    action: Action
+    """A sequence of actions by one character upon another (can be themselves). Used for both player card plays and enemy turns."""
+    actions: list[Action]
     actor: Character
     target: Character
