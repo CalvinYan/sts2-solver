f356dead8e0379d65e8f888c50ac396eeed391c6
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 02:47:00 -0700

    Add Fuzzy Worm Crawler enemy and encounter

--- character/enemy/encounter.py
+++ character/enemy/encounter.py
@@ -1,3 +1,5 @@
-from character.enemy.enemies import Nibbit
+from character.enemy.enemies import FuzzyWurmCrawler, Nibbit
+
+def fuzzy_wurm_crawler(): return [FuzzyWurmCrawler()]
 
 def nibbit(): return [Nibbit()]
---
+++ character/enemy/enemies/fuzzy_wurm_crawler.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from character.enemy import Enemy, Intent
+from util import Action
+from util.effects import Strength
+
+@dataclass
+class FuzzyWurmCrawler(Enemy):
+    intent = AcidGoop1()
+    min_hp = 58
+    max_hp = 59
+
+@dataclass
+class AcidGoop1(Intent):
+    actions = [Action(damage=6, block=0)]
+    
+    def next(self) -> Intent:
+        return Inhale()
+
+@dataclass
+class Inhale(Intent):
+    actions = [Action(damage=0, block=0, buffs=[Strength(power=7)])]
+    
+    def next(self) -> Intent:
+        return AcidGoop2()
+
+@dataclass
+class AcidGoop2(Intent):
+    actions = [Action(damage=6, block=0)]
+    
+    def next(self) -> Intent:
+        return AcidGoop1()
