5b4aa36be1ab8a977d8adc4eb3c3a422ed7d574f
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 02:56:12 -0700

    Add Shrinker Beetle enemy and encounter

--- character/enemy/encounter.py
+++ character/enemy/encounter.py
@@ -1,5 +1,7 @@
-from character.enemy.enemies import FuzzyWurmCrawler, Nibbit
+from character.enemy.enemies import FuzzyWurmCrawler, Nibbit, ShrinkerBeetle
 
 def fuzzy_wurm_crawler(): return [FuzzyWurmCrawler()]
 
 def nibbit(): return [Nibbit()]
+
+def shrinker_beetle(): return [ShrinkerBeetle()]
---
+++ character/enemy/enemies/shrinker_beetle.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from character.enemy import Enemy, Intent
+from util import Action
+from util.effects import Shrink
+
+@dataclass
+class ShrinkerBeetle(Enemy):
+    intent = Shrinker()
+    min_hp = 40
+    max_hp = 42
+
+@dataclass
+class Shrinker(Intent):
+    actions = [Action(damage=0, block=0, debuffs=[Shrink()])]
+    
+    def next(self) -> Intent:
+        return Chomp()
+
+@dataclass
+class Chomp(Intent):
+    actions = [Action(damage=8, block=0)]
+    
+    def next(self) -> Intent:
+        return Stomp()
+
+@dataclass
+class Stomp(Intent):
+    actions = [Action(damage=14, block=0)]
+    
+    def next(self) -> Intent:
+        return Chomp()
