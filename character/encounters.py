d00e76e8c6cc7613f6879a3494e95d9e4d56786d
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 22:52:00 -0700

    Enable verbose logging for enemy encounters

--- character/encounters.py
+++ character/encounters.py
@@ -1,11 +1,11 @@
 from character.enemies import FuzzyWurmCrawler, Nibbit, Seapunk, ShrinkerBeetle, SludgeSpinner
 
-def fuzzy_wurm_crawler(): return [FuzzyWurmCrawler(name="Fuzzy Wurm Crawler")]
+def fuzzy_wurm_crawler(verbose: bool = False): return [FuzzyWurmCrawler(name="Fuzzy Wurm Crawler", verbose=verbose)]
 
-def nibbit(): return [Nibbit(name="Nibbit")]
+def nibbit(verbose: bool = False): return [Nibbit(name="Nibbit", verbose=verbose)]
 
-def seapunk(): return [Seapunk(name="Seapunk")]
+def seapunk(verbose: bool = False): return [Seapunk(name="Seapunk", verbose=verbose)]
 
-def shrinker_beetle(): return [ShrinkerBeetle(name="Shrinker Beetle")]
+def shrinker_beetle(verbose: bool = False): return [ShrinkerBeetle(name="Shrinker Beetle", verbose=verbose)]
 
-def sludge_spinner(): return [SludgeSpinner(name="Sludge Spinner")]
+def sludge_spinner(verbose: bool = False): return [SludgeSpinner(name="Sludge Spinner", verbose=verbose)]
