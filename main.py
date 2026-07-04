74a913b67d669a70c2d76883c804dd631c5a4c9e
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 18:58:09 -0700

    Implement statistical comparison of three Ironclad strategies in main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+from copy import deepcopy
+
 from card import Bash, Defend, Strike
-from character.encounters import nibbit
+from character.encounters import fuzzy_wurm_crawler, nibbit, seapunk, shrinker_beetle, sludge_spinner
+from collections import defaultdict
 from fight import Fight
 from character.player import Ironclad
+from util.core import Move
+
+def incoming_damage(fight: Fight) -> int:
+    dmg = 0
+    for action in fight.enemies[0].intent.actions:
+        if action.damage:
+            new_action = deepcopy(action)
+            move = Move(new_action, actor=fight.enemies[0], target=fight.player)
+            for buff in fight.enemies[0].buffs:
+                buff.resolve(move, is_target=False)
+            for debuff in fight.enemies[0].debuffs:
+                debuff.resolve(move, is_target=False)
+            for buff in fight.player.buffs:
+                buff.resolve(move, is_target=True)
+            for debuff in fight.player.debuffs:
+                debuff.resolve(move, is_target=True)
+            dmg += new_action.damage
+
+    return dmg
 
 # Attack, attack, attack! Prioritize cards in the following order: Bash, Strike, Defend. Spend all energy.
 def unga_bunga_ironclad(fight: Fight) -> bool:
@@ -22,8 +44,62 @@ def unga_bunga_ironclad(fight: Fight) -> bool:
 
     return player.energy == 0 or player.hand.total() == 0  # End turn if no energy left or hand is empty
 
-if __name__ == "__main__":
-    player = Ironclad(name="Ironchad", player_turn_callback=unga_bunga_ironclad)
-    fight = Fight(player=player, enemies=nibbit(), verbose=True)
+# The way an inexperienced player might play - block all incoming damage, then priotize Bash, then Strike.
+def noob_ironclad(fight: Fight) -> bool:
 
-    fight.start()
+    player = fight.player
+    
+    bash = Bash()
+    strike = Strike()
+    defend = Defend()
+
+    if incoming_damage(fight) > player.block and player.hand[defend] > 0 and defend.playable(player.energy):
+        player.play(defend, target=None)
+
+    elif player.hand[bash] > 0 and bash.playable(player.energy):
+        player.play(bash, target=fight.enemies[0])
+
+    elif player.hand[strike] > 0 and strike.playable(player.energy):
+        player.play(strike, target=fight.enemies[0])
+
+    else:
+        return True  # End turn if no cards can be played
+
+    return player.energy == 0 or player.hand.total() == 0  # End turn if no energy left or hand is empty
+
+# A more balanced approach - Play Bash if it's in hand, then block if it would save 5 HP, otherwise play Strike.
+def balanced_ironclad(fight: Fight) -> bool:
+    player = fight.player
+
+    bash = Bash()
+    strike = Strike()
+    defend = Defend()
+
+    if player.hand[bash] > 0 and bash.playable(player.energy):
+        player.play(bash, target=fight.enemies[0])
+
+    elif incoming_damage(fight) >= player.block + 5 and player.hand[defend] > 0 and defend.playable(player.energy):
+        player.play(defend, target=None)
+
+    elif player.hand[strike] > 0 and strike.playable(player.energy):
+        player.play(strike, target=fight.enemies[0])
+    
+    else:
+        return True  # End turn if no cards can be played
+
+    return player.energy == 0 or player.hand.total() == 0  # End turn if no energy left or hand is empty
+
+if __name__ == "__main__":
+
+    hp_losses = defaultdict(list)
+
+    for name, callback in zip(["Chad", "Virgin", "Balanced"], [unga_bunga_ironclad, noob_ironclad, balanced_ironclad]):
+        for _ in range(10000):
+            player = Ironclad(name=name, player_turn_callback=callback)
+            fight = Fight(player=player, enemies=fuzzy_wurm_crawler())
+            fight.start()
+
+            hp_losses[name].append(player.hp)
+
+    for name in ["Chad", "Virgin", "Balanced"]:
+        print(f"Average HP loss of {name}: {sum(hp_losses[name]) / len(hp_losses[name])}")
