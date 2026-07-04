f402cfce6b18719e9c0981eac6c91125fe316955
Calvin Yan <calvinyan@berkeley.edu>
2026-07-03 17:35:23 -0700

    Enable logging in fight.py

--- fight.py
+++ fight.py
@@ -4,21 +4,30 @@ from dataclasses import dataclass
 from character.enemy import Enemy
 from character.player import Player
 
-@dataclass
+@dataclass(repr=False)
 class Fight:
     player: Player
     enemies: list[Enemy]
     turn: int = 0
+    verbose: bool = False
 
     def loop(self) -> None:
         self.turn += 1
 
+        if self.verbose:
+            print(self)
+
         self.player.resolve_start_of_turn(self)
         while not self.player.resolve_turn(self):
-            pass
+            if self.verbose:
+                print(self)
+
         self.player.resolve_end_of_turn(self)
 
         self.enemies = [enemy for enemy in self.enemies if enemy.hp > 0]
+
+        if self.verbose:
+            print(self)
 
         for enemy in self.enemies:
             enemy.resolve_start_of_turn(self)
@@ -27,13 +36,16 @@ class Fight:
         for enemy in self.enemies:
             enemy.resolve_end_of_turn(self)
 
+        if self.verbose:
+            print(self)
+
     def is_over(self) -> bool:
         return all(enemy.hp <= 0 for enemy in self.enemies) or self.turn >= 20
-
-    def __str__(self) -> str:
-        return f"Turn {self.turn}\nPlayer: {self.player}\nEnemies: {self.enemies}\nHP lost: {self.player.hp}"
 
     def start(self) -> None:
         while not self.is_over():
             self.loop()
         print(f"Fight ended after {self.turn} turns. Player HP loss: {self.player.hp}")
+
+    def __repr__(self) -> str:
+        return f"\nTurn {self.turn}\nPlayer: {self.player}\nEnemies: {'\n'.join(repr(enemy) for enemy in self.enemies)}\n"
