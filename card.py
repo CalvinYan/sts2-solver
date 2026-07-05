aa58f58f935f3745bec57173fd4eea5c7481c146
Calvin Yan <calvinyan@berkeley.edu>
2026-07-05 09:53:21 -0700

    Add card IDs and implement Card.to_vector

--- card.py
+++ card.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 
 from util.core import Action
-from util.effects import Vulnerable
+from util.effect import Vulnerable
 
 class Targeting(Enum):
     NONE = 0
@@ -12,6 +12,7 @@ class Targeting(Enum):
 @dataclass(frozen=True, repr=False)
 class Card:
     """An action with an energy cost and a target type."""
+    id: int
     action: Action
     cost: int
     targeting: Targeting
@@ -19,32 +20,48 @@ class Card:
     def playable(self, energy: int) -> bool:
         return energy >= self.cost
 
+    def to_vector(self) -> tuple:
+        base = [0] * (max(ID_TO_CARD.keys()) + 1)
+        base[self.id] = 1
+        return tuple(base)
+
     def __repr__(self):
         return type(self).__name__
 
 @dataclass(frozen=True, repr=False)
 class Strike(Card):
+    id: int = 0
     action: Action = field(default_factory=lambda: Action(damage=6))
     cost: int = 1
     targeting: Targeting = Targeting.ENEMY_SINGLE
 
 @dataclass(frozen=True, repr=False)
 class Defend(Card):
+    id: int = 1
     action: Action = field(default_factory=lambda: Action(block=5))
     cost: int = 1
     targeting: Targeting = Targeting.NONE
 
 @dataclass(frozen=True, repr=False)
 class Bash(Card):
+    id: int = 2
     action: Action = field(default_factory=lambda: Action(damage=8, debuffs=[Vulnerable(duration=2)]))
     cost: int = 2
     targeting: Targeting = Targeting.ENEMY_SINGLE
 
 @dataclass(frozen=True, repr=False)
 class AscendersBane(Card):
+    id: int = 11
     action: Action = field(default_factory=lambda: Action())
     cost: int = None
     targeting: Targeting = Targeting.NONE
 
     def playable(self, energy: int) -> bool:
         return False
+
+ID_TO_CARD = {
+    Strike.id: Strike,
+    Defend.id: Defend,
+    Bash.id: Bash,
+    AscendersBane.id: AscendersBane
+}
